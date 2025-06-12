c0429402192fc774a87efdc570ff3418ff4a096a
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-12 17:32:59 -0500

    first commit

---
+++ src/test/java/com/andresavilesdev/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresavilesdev.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
